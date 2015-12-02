42a18e1bacf6015698537af9a4a667120d146aed
Atticus White <contact@atticuswhite.com>
2015-12-01 22:12:22 -0500

    initial commit

---
+++ src/main/java/com/robinpowered/react/SharedPreferencesModule.java
@@ -0,0 +1,50 @@
+package com.robinpowered.react;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.Promise;
+
+public class SharedPreferencesModule extends ReactContextBaseJavaModule {
+  Activity mActivity = null;
+  SharedPreferences preferences;
+
+  public SharedPreferencesModule(ReactApplicationContext reactApplicationContext, Activity activity) {
+    super(reactApplicationContext);
+    preferences = reactApplicationContext.getApplicationContext().getSharedPreferences("titanium", Context.MODE_PRIVATE);
+    System.out.println("SharedPreferencesModule HAS INITIALIZED");
+    mActivity = activity;
+  }
+
+  @Override
+  public String getName() {
+    return "SharedPreferencesAndroid";
+  }
+
+  @ReactMethod
+  public void getString(String key, Promise promise) {
+    Object value = preferences.getAll().get(key);
+    if (value != null) {
+      promise.resolve(value.toString());
+    } else {
+      promise.resolve("");
+    }
+  }
+
+  @ReactMethod
+  public void setString(String key, String value, Promise promise) {
+    SharedPreferences.Editor editor = preferences.edit();
+    if (value == null) {
+      editor.remove(key);
+    } else {
+      editor.putString(key, value);
+    }
+    editor.commit();
+    promise.resolve("");
+  }
+}
---
+++ src/main/java/com/robinpowered/react/SharedPreferencesPackage.java
@@ -0,0 +1,39 @@
+package com.robinpowered.react;
+
+import android.app.Activity;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SharedPreferencesPackage implements ReactPackage {
+  private Activity mActivity = null;
+
+  public SharedPreferencesPackage(Activity activity) {
+    mActivity = activity;
+  }
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+    List<NativeModule> modules = new ArrayList<NativeModule>();
+    modules.add(new SharedPreferencesModule(reactApplicationContext, mActivity));
+    return modules;
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+    return Arrays.<ViewManager>asList();
+  }
+}
